99634dbbc73bcd09c58b543e50ea4bc693e63ebb
Jerry <chih.chia.chuang@gmail.com>
2019-06-11 21:56:57 +0000

    Complete exercise 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -2,6 +2,13 @@ class Employee < ActiveRecord::Base
   belongs_to :store
   validates :first_name, :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 39, less_than:201 }
+
+  before_save :random_password
+  private 
+
+  def random_password
+    self.password = ('a'..'z').to_a.shuffle[0,8].join
+  end  
 end
 
 
