af74939f5e8f0a2b17b44cdc260eaa6433bb3b09
Jerry <chih.chia.chuang@gmail.com>
2019-06-11 23:25:58 +0000

    Complete exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -13,6 +13,17 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3 }, presence: true
   validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
   validates_with MyValidator
+
+  before_destroy :empty_store
+  private 
+
+  def empty_store
+    if self.employees.count > 0
+      throw(:abort)
+    else 
+      true
+    end
+  end  
 end
 
 
